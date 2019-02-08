cbde75e1c6011cc3658418a9146983da6e7f0b2a
Jesus Sarmiento <jsarmiento1614@gmail.com>
2019-02-08 00:57:04 -0600

    Se ha creado el diseño de la vista Login, faltando mejorar aun el modo responsive

--- src/app/@theme/auth/login/login.component.ts
+++ src/app/@theme/auth/login/login.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { MAT_LABEL_GLOBAL_OPTIONS } from '@angular/material';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
+@NgModule({
+  providers: [
+    {provide: MAT_LABEL_GLOBAL_OPTIONS, useValue: {float: 'always'}}
+  ]
+})
+
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+  options: FormGroup;
+  constructor(fb: FormBuilder) {
+    this.options = fb.group({
+      hideRequired: false,
+      floatLabel: 'auto',
+    });
+  }
+  // Variable For Password
+  hide = true;
   ngOnInit() {
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './@theme/auth/login/login.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './@theme/nav/nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 
 // IMPORT PARA ANGULAR MATERIAL
-import { MatFormFieldModule, MatInputModule } from '@angular/material';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatFormFieldModule, MatInputModule, MatSelectModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    NavComponent
+    RoutingComponents,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,9 +31,13 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     MatIconModule,
     MatListModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatRadioModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
